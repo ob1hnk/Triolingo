c77a1c9357ee932ba5bb34ee6f1b0612be062708
yellowsubmarine372 <95083164+yellowsubmarine372@users.noreply.github.com>
2026-01-19 15:17:57 +0900

    feat: added repository and real firebase implementation

---
+++ GoGoGolem/src/ai/interaction/text/domain/repository/__init__.py
@@ -0,0 +1,6 @@
+from GoGoGolem.src.ai.interaction.text.domain.entity.letter import Letter
+from interaction.text.domain.repository.letter_response import (
+    LetterResponseRepositoryPort,
+)
+
+__all__ = ["Letter", "LetterResponseRepositoryPort"]
