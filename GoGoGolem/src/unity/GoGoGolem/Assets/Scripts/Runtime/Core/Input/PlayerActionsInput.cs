016bd37dcf65c89a27a3c673ca12f00698175456
ob1hnk <frozie711@gmail.com>
2026-02-18 23:15:27 +0900

    remove all namespaces

--- GoGoGolem/src/unity/GoGoGolem/Assets/Scripts/Runtime/Core/Input/PlayerActionsInput.cs
+++ GoGoGolem/src/unity/GoGoGolem/Assets/Scripts/Runtime/Core/Input/PlayerActionsInput.cs
@@ -1,45 +1,42 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace MyAssets.CharacterControllerNamespace
+[DefaultExecutionOrder(-2)]
+public class PlayerActionsInput : MonoBehaviour, GameInputActions.IPlayerActionsActions
 {
-    [DefaultExecutionOrder(-2)]
-    public class PlayerActionsInput : MonoBehaviour, GameInputActions.IPlayerActionsActions
+    #region Class Variables
+    public bool GatherPressed { get; private set; }
+
+    private PlayerLocomotionInput _playerLocomotionInput;
+    #endregion
+
+    #region Startup
+    private void Awake()
     {
-        #region Class Variables
-        public bool GatherPressed { get; private set; }
+        _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+    }
+    
+    #endregion
 
-        private PlayerLocomotionInput _playerLocomotionInput;
-        #endregion
-
-        #region Startup
-        private void Awake()
-        {
-            _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
-        }
-        
-        #endregion
-
-        private void Update()
-        {
-            if (_playerLocomotionInput.MovementInput != Vector2.zero)
-            {
-                GatherPressed = false;
-            }
-        }
-
-        public void SetGatherPressedFalse()
+    private void Update()
+    {
+        if (_playerLocomotionInput.MovementInput != Vector2.zero)
         {
             GatherPressed = false;
         }
+    }
+
+    public void SetGatherPressedFalse()
+    {
+        GatherPressed = false;
+    }
 
 
-        public void OnGather(InputAction.CallbackContext context)
-        {
-            if (!context.performed)
-                return;
+    public void OnGather(InputAction.CallbackContext context)
+    {
+        if (!context.performed)
+            return;
 
-            GatherPressed = true;
-        }
+        GatherPressed = true;
     }
 }
--- GoGoGolem/src/unity/GoGoGolem/Assets/Scripts/Runtime/Core/Input/PlayerLocomotionInput.cs
+++ GoGoGolem/src/unity/GoGoGolem/Assets/Scripts/Runtime/Core/Input/PlayerLocomotionInput.cs
@@ -1,60 +1,57 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace MyAssets.CharacterControllerNamespace
+[DefaultExecutionOrder(-2)]
+public class PlayerLocomotionInput 
+    : MonoBehaviour, GameInputActions.IPlayerMovementActions
 {
-    [DefaultExecutionOrder(-2)]
-    public class PlayerLocomotionInput 
-        : MonoBehaviour, GameInputActions.IPlayerMovementActions
+    [SerializeField] private bool holdToSprint = true;
+
+    public Vector2 MovementInput { get; private set; }
+    public Vector2 LookInput { get; private set; }
+    public bool SprintToggledOn { get; private set; }
+
+    private GameInputActions input;
+
+    private void OnEnable()
     {
-        [SerializeField] private bool holdToSprint = true;
+        input = new GameInputActions();
 
-        public Vector2 MovementInput { get; private set; }
-        public Vector2 LookInput { get; private set; }
-        public bool SprintToggledOn { get; private set; }
+        input.PlayerMovement.SetCallbacks(this);
+        input.PlayerMovement.Enable();
+    }
 
-        private GameInputActions input;
+    private void OnDisable()
+    {
+        if (input == null) return;
 
-        private void OnEnable()
+        input.PlayerMovement.RemoveCallbacks(this);
+        input.PlayerMovement.Disable();
+    }
+
+    // ===== PlayerMovement Actions =====
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        MovementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        LookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnToggleSprint(InputAction.CallbackContext context)
+    {
+        if (!context.performed && !context.canceled) return;
+
+        if (context.performed)
         {
-            input = new GameInputActions();
-
-            input.PlayerMovement.SetCallbacks(this);
-            input.PlayerMovement.Enable();
+            SprintToggledOn = holdToSprint || !SprintToggledOn;
         }
-
-        private void OnDisable()
+        else if (context.canceled)
         {
-            if (input == null) return;
-
-            input.PlayerMovement.RemoveCallbacks(this);
-            input.PlayerMovement.Disable();
-        }
-
-        // ===== PlayerMovement Actions =====
-
-        public void OnMovement(InputAction.CallbackContext context)
-        {
-            MovementInput = context.ReadValue<Vector2>();
-        }
-
-        public void OnLook(InputAction.CallbackContext context)
-        {
-            LookInput = context.ReadValue<Vector2>();
-        }
-
-        public void OnToggleSprint(InputAction.CallbackContext context)
-        {
-            if (!context.performed && !context.canceled) return;
-
-            if (context.performed)
-            {
-                SprintToggledOn = holdToSprint || !SprintToggledOn;
-            }
-            else if (context.canceled)
-            {
-                SprintToggledOn = !holdToSprint && SprintToggledOn;
-            }
+            SprintToggledOn = !holdToSprint && SprintToggledOn;
         }
     }
 }
