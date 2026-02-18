2f66e243d7b31fc1d68ebd3f36ec0fda12235c9d
ob1hnk <frozie711@gmail.com>
2026-02-18 17:26:01 +0900

    item scriptable object
    Former-commit-id: d7d6d43737be09d22ada2184b2f2ae39d89d07f2

--- GoGoGolem/src/unity/GoGoGolem/Assets/Scripts/Runtime/Core/Managers/InventoryManager.cs
+++ GoGoGolem/src/unity/GoGoGolem/Assets/Scripts/Runtime/Core/Managers/InventoryManager.cs
@@ -9,16 +9,14 @@ public class InventoryManager : MonoBehaviour
         Logic = new InventoryLogic();
     }
 
-    // 외부(퀘스트, 충돌체)에서 호출할 메서드
     public void AcquireItem(string itemID)
     {
-        // DB에 존재하는 아이템인지 먼저 확인
         var itemData = Managers.Data.ItemDB.GetItem(itemID);
         if (itemData == null)
         {
             Debug.LogWarning($"DB에 존재하지 않는 아이템 ID: {itemID}");
+            return;
         }
         Logic.AddItem(itemID);
-        Debug.Log($"아이템 획득: {itemData.itemName}");
     }
 }
