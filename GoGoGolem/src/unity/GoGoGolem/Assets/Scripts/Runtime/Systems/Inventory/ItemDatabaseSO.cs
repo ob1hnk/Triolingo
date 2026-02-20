121132edbdc71c498956739737fdeffde67b1952
최은별 <99008137+dmsquf03@users.noreply.github.com>
2026-02-20 21:07:55 +0900

    Revert "Refactor/reorganize code structure"

--- GoGoGolem/src/unity/GoGoGolem/Assets/Scripts/Runtime/Systems/Inventory/ItemDatabaseSO.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemDatabaseSO", menuName = "Inventory/Item Database")]
-public class ItemDatabaseSO : ScriptableObject
-{
-    public List<ItemData> items = new List<ItemData>();
-}
