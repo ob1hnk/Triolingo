031bb4f07898abdb66438d6153653a2bcdd8f286
Eunbyeol Choi <choieunbyeol@ewhain.net>
2026-05-20 21:07:58 +0900

    fix: sleep face on golem at night in Room scene

--- GoGoGolem/src/unity/GoGoGolem/Assets/Scripts/Runtime/Multimodal/Gesture/Animation/GolemFaceController.cs
+++ GoGoGolem/src/unity/GoGoGolem/Assets/Scripts/Runtime/Multimodal/Gesture/Animation/GolemFaceController.cs
@@ -9,10 +9,14 @@ public class GolemFaceController : MonoBehaviour
 {
     public Renderer faceRenderer;
 
+    [Tooltip("밤에 표시할 별도 face 오브젝트. 같은 위치에 배치 후 연결.")]
+    [SerializeField] private Renderer nightFaceRenderer;
+
     public enum GolemEmotion { Neutral, Happy, Sad, Angry, Surprise, Fear }
 
     [SerializeField] private GolemEmotion currentEmotion = GolemEmotion.Neutral;
     [SerializeField] private Color faceColor = Color.black;
+    [SerializeField] private Color nightFaceColor = Color.black;
 
     private Vector2[] faceUVOffsets = new Vector2[]
     {
@@ -27,7 +31,8 @@ public class GolemFaceController : MonoBehaviour
     private void ApplyMaterial(Material mat, GolemEmotion emotion, Color color)
     {
         Vector2 offset = faceUVOffsets[(int)emotion];
-        mat.SetVector("_MainTex_ST", new Vector4(0.333f, 0.5f, offset.x, offset.y));
+        var tiling = new Vector4(0.333f, 0.5f, offset.x, offset.y);
+        mat.SetVector("_MainTex_ST", tiling);
         mat.SetColor("_Color", color);
     }
 
@@ -37,26 +42,52 @@ public class GolemFaceController : MonoBehaviour
             faceRenderer = GetComponent<Renderer>();
 
         ApplyMaterial(faceRenderer.material, currentEmotion, faceColor);
+
+        if (nightFaceRenderer != null)
+        {
+            nightFaceRenderer.gameObject.SetActive(false);
+            ApplyMaterial(nightFaceRenderer.material, currentEmotion, nightFaceColor);
+        }
     }
 
     void OnValidate()
     {
         if (faceRenderer == null)
             faceRenderer = GetComponent<Renderer>();
-
-        if (faceRenderer != null && faceRenderer.sharedMaterial != null)
-            ApplyMaterial(faceRenderer.sharedMaterial, currentEmotion, faceColor);
     }
 
     public void SetFace(GolemEmotion emotion)
     {
         currentEmotion = emotion;
         ApplyMaterial(faceRenderer.material, currentEmotion, faceColor);
+        if (nightFaceRenderer != null)
+            ApplyMaterial(nightFaceRenderer.material, currentEmotion, nightFaceColor);
+    }
+
+    public void SetNightFace(GolemEmotion emotion)
+    {
+        if (nightFaceRenderer == null) return;
+        ApplyMaterial(nightFaceRenderer.material, emotion, nightFaceColor);
     }
 
     public void SetFaceColor(Color color)
     {
         faceColor = color;
         ApplyMaterial(faceRenderer.material, currentEmotion, faceColor);
+    }
+
+    public void FadeToNightColor(float duration = 1.5f)
+    {
+        if (nightFaceRenderer == null) return;
+        SetNightFace(GolemEmotion.Fear);
+        faceRenderer.enabled = false;
+        nightFaceRenderer.gameObject.SetActive(true);
+    }
+
+    public void FadeToDefaultColor(float duration = 1.5f)
+    {
+        if (nightFaceRenderer == null) return;
+        nightFaceRenderer.gameObject.SetActive(false);
+        faceRenderer.enabled = true;
     }
 }
